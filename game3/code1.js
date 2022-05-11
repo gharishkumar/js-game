82c830d26da4faa298fde1b14de7660343c334a2
Harish Kumar <g.harishkumar1998@gmail.com>
2022-05-11 18:02:52 +0530

    3

---
+++ game3/code1.js
@@ -0,0 +1,20 @@
+gdjs.Level_322Code = {};
+
+gdjs.Level_322Code.conditionTrue_0 = {val:false};
+gdjs.Level_322Code.condition0IsTrue_0 = {val:false};
+
+
+gdjs.Level_322Code.eventsList0 = function(runtimeScene) {
+
+};
+
+gdjs.Level_322Code.func = function(runtimeScene) {
+runtimeScene.getOnceTriggers().startNewFrame();
+
+
+gdjs.Level_322Code.eventsList0(runtimeScene);
+return;
+
+}
+
+gdjs['Level_322Code'] = gdjs.Level_322Code;
